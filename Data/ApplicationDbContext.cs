e16c5a47cbc97b22e439bc90a99045ee42d35d3c
SartJ <sartajjc@gmail.com>
2022-07-22 23:41:13 +0600

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookwormWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookwormWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
